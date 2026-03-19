7597c1cf753c26c3efb372176acb6fd4bf86cb0f
aagrawal66 <aagrawal66@utexas.edu>
2026-03-19 15:04:09 -0500

    cleaned up offset calcs

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ This repo can be used for the following:
 * Vicon and image data collection of defined Vicon objects
 * Camera intrinsics calibration using OpenCV functions and Charuco board
 * Truth and Vicon frame offset optimization using Scipy non-linear least squares solver
+
+
+# Setup `sc-pose-utils` dependency 
+```bash
+git clone https://github.com/aagrawal66/sc-pose-utils.git
+cd sc-pose-utils
+python -m pip install -e . # installs into "activated" python
+```
--- src/cam_offset.py
+++ src/cam_offset.py
@@ -14,114 +14,10 @@ from pathlib import Path
 import pdb
 
 
+# Local imports:
+from sc_pose.mathtils.quaternion import q2rotm
 
-######## Inputs ################
-HERE                    = Path(__file__).resolve().parent
-REPO_ROOT               = HERE.parent
-EXAMPLES_ROOT           = REPO_ROOT.parent / "sc-pose-utils" / "src" / "sc_pose" / "examples"
-
-DATA_FOLDER             = EXAMPLES_ROOT / "artifacts" / "offset" / "expm_003"
-DATA_NAME               = DATA_FOLDER.name
-IMAGE_DIR               = DATA_FOLDER / "images"
-IMG_SUFFIX              = ".png"
-VICON_CSV_PATH          = DATA_FOLDER / "vicon_data.csv"
-CALIBRATION_YAML_PATH   = DATA_FOLDER / "calibration.yaml"
-
-RESULT_PATH             = HERE / "results" / 'test_001'
-OPENCV_POSE_EST_PATH    = RESULT_PATH / "calc_camera_poses.csv"
-OUTPUT_JSON_PATH        = RESULT_PATH / "offset_results.json"
-REPROJECTION_DIR        = RESULT_PATH / "reprojection"
-
-SQUARES_X       = 9          # columns
-SQUARES_Y       = 5          # rows
-SQUARE_LEN      = 17e-3      # meters
-MARKER_LEN      = 12e-3      # meters
-DICTIONARY      = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_100)
-
-########### Load Camera Intrinsics and Distortion Coeffiecients  ############
-if os.path.exists(RESULT_PATH):
-    print(f"Warning: {RESULT_PATH} already exists. Deleting and recreating.")
-    shutil.rmtree(RESULT_PATH)
-RESULT_PATH.mkdir(parents = True, exist_ok = True)
-REPROJECTION_DIR.mkdir(parents = True, exist_ok = True)
-IMAGE_PATH  = str(IMAGE_DIR)  # e.g. "data/images"
-
-with open(CALIBRATION_YAML_PATH) as f:
-    data = yaml.safe_load(f)
-
-K = np.array([
-    [data['fx'], 0,            data['cx']],
-    [0,            data['fy'], data['cy']],
-    [0,            0,            1]
-], dtype=np.float64)
-
-dist = np.array([
-    data['k1'],
-    data['k2'],
-    data['p1'],
-    data['p2'],
-    data['k3']
-], dtype=np.float64).reshape(1, 5)
-
-
-# --------------------------------------------------------------
-# Load and Read Vicon CSV File 
-# ---------------------------------------------------------------
-
-with open(VICON_CSV_PATH, newline='') as f:
-    rows = list(csv.DictReader(f))
-
-image_numbers = [int(r['image_number']) for r in rows]
-
-cam_VCv_V       = np.array([[float(r['cam_x']),  float(r['cam_y']),  float(r['cam_z'])]  for r in rows])/1000
-soho_VTv_V      = np.array([[float(r['soho_x']), float(r['soho_y']), float(r['soho_z'])] for r in rows])/1000
-q_V_Cv          = np.array([[float(r['cam_qw']),  float(r['cam_qx']),  float(r['cam_qy']),  float(r['cam_qz'])]  for r in rows])
-q_V_Tv          = np.array([[float(r['soho_qw']), float(r['soho_qx']), float(r['soho_qy']), float(r['soho_qz'])] for r in rows])
-rotations_cam   = R.from_quat(q_V_Cv,  scalar_first=True).as_matrix()
-rotations_soho  = R.from_quat(q_V_Tv, scalar_first=True).as_matrix()
-from sc_pose.mathtils.quaternion import q2trfm, q2rotm
-Trfms_Cv_V      = np.zeros((len(q_V_Cv), 3, 3))
-Trfms_Tv_V      = np.zeros((len(q_V_Tv), 3, 3))
-for i in range(len(q_V_Cv)):
-    Trfms_Cv_V[i] = q2rotm(q_V_Cv[i])
-    Trfms_Tv_V[i] = q2rotm(q_V_Tv[i])
-sc1             = np.max(Trfms_Cv_V - rotations_cam)
-sc2             = np.max(Trfms_Tv_V - rotations_soho)
-print(f"Max difference between q2rotm and R.from_quat for camera: {sc1:.18f}")
-print(f"Max difference between q2rotm and R.from_quat for target: {sc2:.18f}")
-# ---------------------------------------------------------------
-# Build 4x4 Transforms from Vicon Data
-# ---------------------------------------------------------------
-
-def build_transformations(rotations, translations):
-    N            = len(rotations)
-    T            = np.zeros((N, 4, 4))
-    T[:, :3, :3] = rotations
-    T[:, :3,  3] = translations
-    T[:,  3,  3] = 1.0
-    return T
-
-
-def inv_T(T):
-   T_inv = np.eye(4)
-   R_transpose = T[:3, :3].T
-   T_inv[:3, :3] = R_transpose
-   T_inv[:3, 3] = -R_transpose @ T[:3, 3]
-   return T_inv
-
-T_CvV   = build_transformations( Trfms_Cv_V, cam_VCv_V   )  # (N, 4, 4)
-T_TvV   = build_transformations( Trfms_Tv_V, soho_VTv_V )  # (N, 4, 4)
-
-# ---------------------------------------------------------------
-# ChArUco Board Setup
-# ---------------------------------------------------------------
-
-board       = cv2.aruco.CharucoBoard((SQUARES_X, SQUARES_Y),
-                                     SQUARE_LEN,
-                                     MARKER_LEN,
-                                     DICTIONARY)
-detector    = cv2.aruco.CharucoDetector(board)
-
+############################# Helper Functions #############################
 def get_camera_to_board_pose(image, K, dist):
     img     = cv2.imread(image)
     gray    = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -158,10 +54,10 @@ def get_camera_to_board_pose(image, K, dist):
 # Obtain a stack of the T_CB transformation for all of the images
 def get_TBC_series(image_dir, K, dist):
     image_paths = sorted(
-        glob.glob(os.path.join(image_dir, "*.png")),
-        key=lambda x: int(re.search(r'(\d+)', Path(x).stem).group(1))
-    )
-    T_BC_list, valid_image_numbers = [], []
+                            glob.glob(os.path.join(image_dir, "*.png")),
+                            key = lambda x: int(re.search(r'(\d+)', Path(x).stem).group(1))
+                        )
+    T_BC_list, valid_image_numbers  = [], []
 
     for path in image_paths:
         img_num = int(re.search(r'(\d+)', Path(path).stem).group(1))  # e.g. 1,2,3,5...
@@ -174,10 +70,10 @@ def get_TBC_series(image_dir, K, dist):
 
     print(f"Processed {len(T_BC_list)} / {len(image_paths)} images successfully")
     return np.stack(T_BC_list), valid_image_numbers
+
 # ---------------------------------------------------------------
 # RWHE Helpers
 # ---------------------------------------------------------------
-
 def rvec_to_T(rvec, tvec):
     """
     Converts Rodrigues vector + translation into a 4x4 transform.
@@ -185,7 +81,7 @@ def rvec_to_T(rvec, tvec):
     from the flat 12-parameter vector.
     """
     T            = np.eye(4)
-    T[:3, :3], _ = cv2.Rodrigues(np.array(rvec, dtype=np.float64))
+    T[:3, :3], _ = cv2.Rodrigues(np.array(rvec, dtype = np.float64))
     T[:3,  3]    = np.array(tvec, dtype=np.float64).flatten()
     return T
 
@@ -211,23 +107,13 @@ def residuals(params, T_BC_array, T_CvV_array, T_TvV_array):
     res     = []
 
     for T_BC_obs, T_CvV, T_TvV in zip(T_BC_array, T_CvV_array, T_TvV_array):
-
-        # T_CCv = inv_T(T_CvC)
-        # # 1. Prediction Chain
-        # T_TvCv = inv_T(T_VCv) @ T_VTv
-        # T_BC_pred = T_CCv @ T_CvTv @ T_TvT
-
-        # # 2. Matrix Difference (Algebraic)
-        # # We only use the top 3x4 block because the bottom row is [0,0,0,1] 
-        # # for both, meaning the difference is always zero (useless for the solver).
-        
+        # 1. prediction chain        
         T_BC_pred   = T_CvC @ (inv_T(T_CvV) @ T_TvV) @ inv_T(T_TvT)
+        # 2. residual is the difference between the observed T_BC and the predicted T_BC, 3x4 matrix
         diff        = T_BC_obs[:3, :] - T_BC_pred[:3, :]
-        
-        # 3. Flatten into 12 residuals per observation
-        # The sum of squares of these 12 values == Frobenius Norm Squared
+        # 3. flatten into 12 residuals per observation
+        # the sum of squares of these 12 values == Frobenius Norm Squared
         res.append(diff.flatten())
-
     return np.concatenate(res)
 
 
@@ -243,22 +129,130 @@ def solve_rwhe(T0_flat, T_BC_array, T_CvV_array, T_TvV_array):
     """
     print(f"Solving RWHE with {len(T_BC_array)} measurements...")
 
-    res0 = residuals(T0_flat, T_BC_array, T_CvV_array, T_TvV_array)
+    res0    = residuals(T0_flat, T_BC_array, T_CvV_array, T_TvV_array)
     print(f"Initial cost: {0.5 * np.sum(res0**2):.6f}")
 
     result = least_squares(
-        residuals,
-        T0_flat,
-        args=(T_BC_array, T_CvV_array, T_TvV_array),
-        method='lm',
-        verbose=1
-    )
+                            residuals,
+                            T0_flat,
+                            args = (T_BC_array, T_CvV_array, T_TvV_array),
+                            method = 'lm',
+                            verbose = 1
+                        )
 
     print(f"Final cost:  {result.cost:.6f}")
     print(f"Termination: {result.message}")
 
-    T_CvC, T_TvT = params_to_T(result.x[:6]), params_to_T(result.x[6:])
+    T_CvC, T_TvT    = params_to_T(result.x[:6]), params_to_T(result.x[6:])
     return T_CvC, T_TvT, result
+############################# Helper Functions #############################
+
+
+############################# Inputs #############################
+HERE                    = Path(__file__).resolve().parent
+PARENT                  = HERE.parent
+DATA_ROOT               = PARENT / "data" / "offset"
+EXP_NAME                = "collection_001"
+DATA_FOLDER             = DATA_ROOT / EXP_NAME
+IMAGE_DIR               = DATA_FOLDER / "images"
+IMG_SUFFIX              = ".png"
+VICON_CSV_PATH          = DATA_FOLDER / "vicon_data.csv"
+CALIBRATION_YAML_PATH   = DATA_FOLDER / "calibration.yaml"
+
+RESULT_PATH             = PARENT / "results" / EXP_NAME
+OPENCV_POSE_EST_PATH    = RESULT_PATH / "calc_camera_poses.csv"
+OUTPUT_JSON_PATH        = RESULT_PATH / "calc_offset_results.json"
+REPROJECTION_DIR        = RESULT_PATH / "reprojection"
+
+SQUARES_X               = 9          # columns
+SQUARES_Y               = 5          # rows
+SQUARE_LEN              = 17e-3      # meters
+MARKER_LEN              = 12e-3      # meters
+DICTIONARY              = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_100)
+############################# Inputs #############################
+
+########### Load Camera Intrinsics and Distortion Coeffiecients  ############
+if os.path.exists(RESULT_PATH):
+    print(f"Warning: {RESULT_PATH} already exists. Deleting and recreating.")
+    shutil.rmtree(RESULT_PATH)
+RESULT_PATH.mkdir(parents = True, exist_ok = True)
+REPROJECTION_DIR.mkdir(parents = True, exist_ok = True)
+IMAGE_PATH  = str(IMAGE_DIR)  # e.g. "data/images"
+
+with open(CALIBRATION_YAML_PATH) as f:
+    data = yaml.safe_load(f)
+
+K   = np.array([
+                    [data['fx'],        0.0,                data['cx']],
+                    [0.0,               data['fy'],         data['cy']],
+                    [0.0,               0.0,                1.0]
+                ], dtype = np.float64)
+
+dist    = np.array([
+                        data['k1'],
+                        data['k2'],
+                        data['p1'],
+                        data['p2'],
+                        data['k3']
+                    ], dtype = np.float64).reshape(1, 5)
+
+# --------------------------------------------------------------
+# Load and Read Vicon CSV File 
+# ---------------------------------------------------------------
+
+with open(VICON_CSV_PATH, newline='') as f:
+    rows    = list(csv.DictReader(f))
+
+image_numbers   = [int(r['image_number']) for r in rows]
+
+cam_VCv_V       = np.array([[float(r['cam_x']),  float(r['cam_y']),  float(r['cam_z'])]  for r in rows])/1000
+soho_VTv_V      = np.array([[float(r['soho_x']), float(r['soho_y']), float(r['soho_z'])] for r in rows])/1000
+q_V_Cv          = np.array([[float(r['cam_qw']),  float(r['cam_qx']),  float(r['cam_qy']),  float(r['cam_qz'])]  for r in rows])
+q_V_Tv          = np.array([[float(r['soho_qw']), float(r['soho_qx']), float(r['soho_qy']), float(r['soho_qz'])] for r in rows])
+rotations_cam   = R.from_quat(q_V_Cv,  scalar_first=True).as_matrix()
+rotations_soho  = R.from_quat(q_V_Tv, scalar_first=True).as_matrix()
+Trfms_Cv_V      = np.zeros((len(q_V_Cv), 3, 3))
+Trfms_Tv_V      = np.zeros((len(q_V_Tv), 3, 3))
+for i in range(len(q_V_Cv)):
+    Trfms_Cv_V[i] = q2rotm(q_V_Cv[i])
+    Trfms_Tv_V[i] = q2rotm(q_V_Tv[i])
+sc1             = np.max(Trfms_Cv_V - rotations_cam)
+sc2             = np.max(Trfms_Tv_V - rotations_soho)
+print(f"Max difference between q2rotm and R.from_quat for camera: {sc1:.18f}")
+print(f"Max difference between q2rotm and R.from_quat for target: {sc2:.18f}")
+# ---------------------------------------------------------------
+# Build 4x4 Transforms from Vicon Data
+# ---------------------------------------------------------------
+
+def build_transformations(rotations, translations):
+    N            = len(rotations)
+    T            = np.zeros((N, 4, 4))
+    T[:, :3, :3] = rotations
+    T[:, :3,  3] = translations
+    T[:,  3,  3] = 1.0
+    return T
+
+
+def inv_T(T):
+   T_inv            = np.eye(4)
+   R_transpose      = T[:3, :3].T
+   T_inv[:3, :3]    = R_transpose
+   T_inv[:3, 3]     = -R_transpose @ T[:3, 3]
+   return T_inv
+
+T_CvV   = build_transformations( Trfms_Cv_V, cam_VCv_V   )  # (N, 4, 4)
+T_TvV   = build_transformations( Trfms_Tv_V, soho_VTv_V )  # (N, 4, 4)
+
+# ---------------------------------------------------------------
+# ChArUco Board Setup
+# ---------------------------------------------------------------
+
+board       = cv2.aruco.CharucoBoard((SQUARES_X, SQUARES_Y),
+                                     SQUARE_LEN,
+                                     MARKER_LEN,
+                                     DICTIONARY)
+detector    = cv2.aruco.CharucoDetector(board)
+
 
 ####### Manually Measured Offset Estimation for Initialization  ###########
 T0_cam              = np.eye(4)
@@ -304,7 +298,7 @@ print(f"T_BC: {len(T_BC_sync)}, T_CvV: {len(T_CvV_sync)}, T_TvV: {len(T_TvV_sync
 
 
 # 3. Solve
-T_CvC, T_TvT, result = solve_rwhe(T0_flat, T_BC_sync, T_CvV_sync, T_TvV_sync)
+T_CvC, T_TvT, result    = solve_rwhe(T0_flat, T_BC_sync, T_CvV_sync, T_TvV_sync)
 print("\n Homogenous Transformation Matrix from Camera Vicon to True Camera Frame:")
 print(T_CvC)
 
@@ -313,14 +307,15 @@ print(T_TvT)
 
 with open(OUTPUT_JSON_PATH, "w") as f:
     json.dump({
-        "T_CvC": T_CvC.tolist(),
-        "T_TvT": T_TvT.tolist()
-    }, f, indent=4)
+                "T_CvC": T_CvC.tolist(),
+                "T_TvT": T_TvT.tolist()
+            }, f, indent = 4
+            )
 
 print(result.fun.shape)
 # Residual per observation (should be small and uniform)
-res = result.fun.reshape(-1, 12)  # 12 residuals per image
-per_obs_cost = np.sum(res**2, axis=1)
+res             = result.fun.reshape(-1, 12)  # 12 residuals per image
+per_obs_cost    = np.sum(res**2, axis = 1)
 print("Per-observation costs:")
 for i, c in enumerate(per_obs_cost):
     print(f"  Image {i}: {c:.6f}")
@@ -330,4 +325,5 @@ mean_cost = np.mean(per_obs_cost)
 for i, c in enumerate(per_obs_cost):
     if c > 3 * mean_cost:
         print(f"  WARNING: Image {i} is an outlier ({c:.4f} vs mean {mean_cost:.4f})")
-print(f"Results saved to {RESULT_PATH} with offets at {OUTPUT_JSON_PATH}")
+print(f"Final Cost: {result.cost:.6f}")
+print(f"Results saved to {RESULT_PATH} with offsets at {OUTPUT_JSON_PATH}")
